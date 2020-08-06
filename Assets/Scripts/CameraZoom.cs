2c1016e43f5e824fb84caa957b9604e53f1cce5b
Gabriel Gindri <gabrielgindridev@gmail.com>
2020-08-06 15:54:20 -0300

    Add Camera to agent and Camera zoom

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] float sensitivity = 5f;
+
+    void Update()
+    {
+        transform.localPosition += Vector3.up * Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+    }
+}
